2cd68719b9eb0a4d5c1eaad2419a4c1cf556f04c
Christian Baars <Baars@gmx.de>
2022-07-07 20:41:47 +0200

    use base64

--- ulp_examples/ulp_I2C_BH1750.py
+++ ulp_examples/ulp_I2C_BH1750.py
@@ -2,7 +2,7 @@
 BH1750 example with exporting ULP code to Tasmotas Berry implementation
 """
 from esp32_ulp import src_to_binary
-import re
+import ubinascii
 
 source = """
 
@@ -472,16 +472,10 @@ print(new_source)
 binary = src_to_binary(new_source)
 
 # Export section for Berry
-code  = ""
-for l in binary:
-    l = hex(l)
-    if(len(l)<4):
-        l = "0x0" + l[-1]
-    code += l[2:]
-print(code)
+code_b64 = ubinascii.b2a_base64(binary).decode('utf-8')[:-1]
 
 file = open ("ulp_template.txt", "w")
-file.write(code)
+file.write(code_b64)
 
 print("")
 # For convenience you can add Berry commands to rapidly test out the resulting ULP code in the console
@@ -490,7 +484,7 @@ print("import ULP")
 print("ULP.wake_period(0,3 * 1000 * 1000)")
 print("ULP.gpio_init(32,0)")
 print("ULP.gpio_init(33,0)")
-print("var c = bytes(\""+code+"\")")
+print("var c = bytes().fromb64(\""+code_b64+"\")")
 print("ULP.load(c)")
 print("ULP.run()")
 

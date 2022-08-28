3938acb27d19a270cc3ba44387e5feedb59391cf
Christian Baars <Baars@gmx.de>
2022-08-28 21:23:42 +0200

    prepare for RISCV ULP
    Not implemented yet in Tasmota for S2 and S3.
    Let's hope it will work in the not so distant future.

--- ulp_helper/binS2Berry.py
+++ ulp_helper/binS2Berry.py
@@ -29,12 +29,15 @@ def get_files():
 def parse_map_file(map_file):
     print("Parsing map file ...")
     global_vars = []
+    ulp_type = "FSM"
     with open(map_file, 'r', encoding='UTF-8') as file:
         while (line := file.readline()):
             if "PROVIDE (ulp_" in line:
-                    m = line[line.find('(')+1:line.find(')')]
-                    global_vars.append(m)
-    return global_vars
+                m = line[line.find('(')+1:line.find(')')]
+                global_vars.append(m)
+            if "ulp_riscv_run" in line:
+                ulp_type = "RISCV"
+    return global_vars,ulp_type
 
 
 def parse_asm_file(asm_file):
@@ -62,16 +65,17 @@ def parse_asm_file(asm_file):
     code_b64 = b64encode(bytes.fromhex(code)).decode()
     return code, code_b64, code_size
     
-def print_output(code,code_b64,code_size,global_vars):
+def print_output(code,code_b64,code_size,global_vars,ulp_type):
     print("### Global vars (including function labels):")
     for var in global_vars:
         rtc_addr = int((int(var.split(" ")[-1],0)-0x50000000)/4)
-        if rtc_addr == 0:
-            print("!!! Make sure, that the next line is a jump to the global entry point or the entry point itself !!!")
+        if rtc_addr == 0 and ulp_type == "FSM":
+            print("# !!! Make sure, that the next line is a jump to the global entry point or the entry point itself !!!")
         var = var.replace("ulp_","")
         print("#",var," -> ULP.get_mem(",rtc_addr,")")
     print("")
-    print("#You can paste the following snippet into Tasmotas Berry console:")
+    print("# ULP-Coprocessor-type:", ulp_type)
+    print("# You can paste the following snippet into Tasmotas Berry console:")
     print("import ULP")
     print("ULP.wake_period(0,1000 * 1000)")
     # print("ULP.gpio_init(32,0)") # only example for setup functions ...
@@ -86,8 +90,8 @@ def main(args):
     asm_file, map_file = get_files()
     print(asm_file, map_file)
     code, code_b64, code_size = parse_asm_file(asm_file)
-    global_vars = parse_map_file(map_file)
-    print_output(code, code_b64, code_size, global_vars)
+    global_vars,ulp_type = parse_map_file(map_file)
+    print_output(code, code_b64, code_size, global_vars,ulp_type)
 
 
 if __name__ == '__main__':
